488b767b0a7892fe738cc7a69e83450a576e1e7d
ANGIELOUX <angelajohnson943@gmail.com>
2022-05-24 15:18:02 +1000

    Clean up base layout/structure, add nav & main components

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Main from './Main.jsx';
+import Nav from './Nav.jsx'
+
+function App() {
+  return (
+   <div>
+     <Nav/>
+     <Main/>
+   </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Main = () => {
+    const name = "AJ";
+    return (
+        <main>
+            <h1>hello world! my name is {name}</h1>
+        </main>
+    )
+}
+
+export default Main
