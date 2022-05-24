fb8d053a0df2c3ebb37faf703a57d432c64c2b11
ANGIELOUX <angelajohnson943@gmail.com>
2022-05-24 16:01:30 +1000

    Add conditional rendering example techniques

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,11 +2,51 @@ import React from 'react';
 
 const Main = () => {
     const name = "AJ";
+    const loading = false
+
+
+    // classic if else statement
+    // if (loading) {
+    //     return null
+    // } else {
+    //     return (
+    //         <main>
+    //             <h1>Hi my name is {name}</h1>
+    //         </main>
+    //     )
+    // }
+
+
+    // ternary operator
+    
+    // return loading ? null : (
+    //     <main>
+    //         <h1>hello world! my name is {name}</h1>
+    //     </main>
+    // )
+
+
+    // short circuit logic --> will return the second part of && 
+    // return !loading && (
+    //     <main>
+    //         <h1>Hi my name is {name}</h1>
+    //     </main>
+    // )
+
+
+    // another short circut syntax
+
     return (
         <main>
-            <h1>hello world! my name is {name}</h1>
+            {loading ? (
+                <h1>Loading...</h1>
+            ) : (
+                <h1>Hi my name is {name}</h1>
+
+            )}
         </main>
     )
+
 }
 
 export default Main
